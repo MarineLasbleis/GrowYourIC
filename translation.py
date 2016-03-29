612be7cff9ea4bf909c6e8e315b172a63c24b509
MarineLasbleis <marine.lasbleis@gmail.com>
2016-03-29 21:46:31 +0900

    translation on bottom point working correctly.

--- translation.py
+++ translation.py
@@ -17,10 +17,7 @@ import geodynamic
 
 
 
-def Translation_analytic_solution():
-
-    age_IC = 1.e6 #in years
-    velocity_translation = 2.01*1221./1.e6 #in km/years 
+def Translation_analytic_BT(velocity_translation = 2.01*1221./1.e6, direction=positions.Cartesian_Point(1,0,0)):
 
     #Initialize map
     m, fig = plot_data.setting_map()
@@ -29,26 +26,21 @@ def Translation_analytic_solution():
     #seismic data set (from Lauren's file)
     data_points = read_write.read_from_file("results.dat", slices=["PKIKP-PKiKP travel time residual", "turn lat", "turn lon", "turn depth"])
     nlines, ncolumns = data_points.shape
-
+    print data_points.info
     #translate it in the correct format
     dataset = []
     translation_dataset = []
-    phi, theta, age = [], [], []
+    phi, theta, age, dt = [], [], [], []
     print nlines, "points to write."
     for i in range(nlines):
         if i%100==0: #print every 100 values
             print "Writing point ",  i, ". Coordinates: ", data_points.ix[i]
-        # translate data in the correct format
-        #dataset.append(positions.Raypath("BT-point",
-        #                                 1221.-data_points.ix[i,"turn depth"],
-        #                                 data_points.ix[i, "turn lat"],
-        #                                 data_points.ix[i, "turn lon"]))
-        # set up the geodynamical model
+
         # because it's analytical solution, we don't need to define a grid. model is calulated exactly. 
         translation_dataset.append(geodynamic.Translation(positions.Seismo_Point(1221.-data_points.ix[i,"turn depth"],
                                                           data_points.ix[i, "turn lat"],
                                                           data_points.ix[i, "turn lon"])))
-        translation_dataset[i].analytical(velocity_translation, positions.Seismo_Point(1, 0, 40))
+        translation_dataset[i].analytical(velocity_translation, direction)
 
 
         phi.append(translation_dataset[i].initial_position.phi)
@@ -58,19 +50,23 @@ def Translation_analytic_solution():
     x, y = m(phi, theta)
     m.scatter(x, y, c=age, zorder=10, cmap=plt.cm.RdYlGn)
     m.colorbar()
+
+    fig, ax = plt.subplots()
+    ax.plot(phi, age/max(age), ".")
+    dt = data_points["PKIKP-PKiKP travel time residual"]
+    print dt.shape
+    ax.plot(phi, dt,".r")
+
     plt.show()
-
-    ## x, y = m(phi, theta)
-    ## m.scatter(x, y, c=(data_points[:,0]-min(data_points[:,0]))*1, zorder=10, cmap=plt.cm.RdYlGn)
-    ## m.colorbar()
-
-    ## plt.show()
 
 
 
 if __name__ == '__main__':
 
+    age_IC = 1.e6 #in years
+    velocity_translation = 2.01*1221./1.e6 #in km/years
+    direction = positions.Seismo_Point(1, 0, 80)
 
-    Translation_analytic_solution()
+    Translation_analytic_BT(velocity_translation, direction)
 
 
