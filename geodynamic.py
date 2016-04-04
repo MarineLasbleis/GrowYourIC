292629fc56831eaad0d38d812b572b43906a6d5c
MarineLasbleis <marine.lasbleis@gmail.com>
2016-04-04 14:45:30 +0900

    comments on geodynamic.py

--- geodynamic.py
+++ geodynamic.py
@@ -27,34 +27,43 @@ def exact_translation(point, velocity, direction=positions.CartesianPoint(1,0,0)
     return max(solution_1, solution_2)
 
 
-class Point_evolution():
+class Model():
     """ Geodynamical modelling of a flow. """
 
-    def __init__(self, Point, time=0.):
+    def __init__(self, Point, time=0., R0=0.):
         """ Point is a positions.Point """
-        self.initial_position = Point
+        self.points = []
         self.old = []
-        self.new = Point
+        self.new = [] 
         self.time = time
-        self.inside = True
         self.history = []
+        self.radius = R0
 
-    def advection(self):
+    def advect(self):
         """ advection of the point. Need to be implemented in the derived classes """
         raise NotImplementedError("need to implement advection() in derived class!")
 
+    def grow(self):
+        """ grow the radius of the core """
+        raise NotImplementedError("need to implement grow() in derived class!")
+
     def check_inside(self):
+        """ check if the points are inside or outside the core"""
         pass
 
-    def translation(self, velocity, dt):
-        """ translation of the point.
+    def add_points(self):
+        """ add points where there are few of them and on the surface if ic is growing """
+        pass
 
-        velocity: tupple with 3 components (cartesian coordinates. x,y,z)
-        """
-        self.new.x = self.old.x + velocity*dt
+    #def translation(self, velocity, dt):
+    #    """ translation of the point.
+    #
+    #         velocity: tupple with 3 components (cartesian coordinates. x,y,z)
+    #       """
+    #       self.new.x = self.old.x + velocity*dt
 
 class Translation(Point_evolution):
-
+    ## Not up-to-date
     ### the __init__ method is directly obtained from the class Point_evolution, so no need to redefined it in a derived class
     ## def __init__(self, Point, time)
     ##     Point_evolution.__init__(self, Point, time)
