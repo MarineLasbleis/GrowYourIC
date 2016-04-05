71dcc6e6d1f649780e2111daed6d8471e2fa0b1f
MarineLasbleis <marine.lasbleis@gmail.com>
2016-04-05 11:39:41 +0900

    initialisation with random sampling of a seed IC

--- geodynamic.py
+++ geodynamic.py
@@ -8,6 +8,7 @@
 import numpy as np
 import matplotlib.pyplot as plt #for figures
 from mpl_toolkits.basemap import Basemap #to render maps
+import math
 
 #personal routines
 import positions
@@ -27,17 +28,48 @@ def exact_translation(point, velocity, direction=positions.CartesianPoint(1,0,0)
     return max(solution_1, solution_2)
 
 
+def numberpoints_surface(r, density):
+    """ return the number of points needed to cover the surface at radius 'r' with a density 'density'
+    
+    r: radius (radius = 1 correspond to IC now)
+    density: density of points at the surface (corresponding to r=1)
+    """
+    return int(4.*np.pi*r**2.*density)
+
+
 class Model():
     """ Geodynamical modelling of a flow. """
 
-    def __init__(self, Point, time=0., R0=0.):
+    def __init__(self):
         """ Point is a positions.Point """
         self.points = []
         self.old = []
         self.new = [] 
-        self.time = time
+        self.time = None
         self.history = []
+        self.radius = None
+        self.Npoints = None
+        self.density = None
+
+    def initialisation(self, t0, R0, density):
+        self.time = t0
         self.radius = R0
+        self.density = density
+        self.fill_sphere(density) #fill the initial sphere with points. If the initial sphere is too small, only the point in r=0 is set up.
+        self.Npoints = len(self.points)
+
+    def run(self, tmax, dt):
+        t0 = self.time
+        for t in np.arange(t0, tmax, dt):
+            self.old = self.points
+            self.time = t
+            self.advect()
+            self.grow()
+            self.add_points()
+            self.check_inside()
+            self.Npoints = len(self.points)
+            
+
 
     def advect(self):
         """ advection of the point. Need to be implemented in the derived classes """
@@ -49,11 +81,55 @@ class Model():
 
     def check_inside(self):
         """ check if the points are inside or outside the core"""
-        pass
+        # find points outside
+        index_outside = [] #store the index of points to remove
+        for i, point in enumerate(self.points):
+            if point.r > rICB:
+                index_outside.append(i)
+        # remove them (store?)
+        self.points = np.delete(self.points, index_outside)
 
-    def add_points(self):
+    def extract_rtp(self):
+        r, theta, phi = np.empty([self.Npoints,1]), np.empty([self.Npoints,1]), np.empty([self.Npoints,1])
+        for i, point in enumerate(self.points):
+            r[i] = point.r
+            theta[i] = point.theta
+            phi[i] = point.phi
+        return r, theta, phi
+
+    def extract_xyz(self):
+        x, y, z = np.empty([self.Npoints,1]), np.empty([self.Npoints,1]), np.empty([self.Npoints,1])
+        for i, point in enumerate(self.points):
+            x[i] = point.x
+            y[i] = point.y
+            z[i] = point.z
+        return x, y, z 
+
+    def add_points(self, density):
         """ add points where there are few of them and on the surface if ic is growing """
-        pass
+        # on the surface
+
+    def fill_sphere(self, density):
+        volume = 4./3. * np.pi *self.radius**3. #volume of the sphere
+        print "volume", volume
+        n = int(math.ceil(density*volume))
+        print n
+        if n == 1:
+            self.points.append(positions.SeismoPoint(0.,0.,0.)) #central point
+        else:
+            # to get a uniform repartition, make it uniform in the (x, y, z) space, but with more points that needed, and then remove all the extra points. Check if at least one point is in the space, and if not add a point at the center. 
+            n = int(math.ceil(density*volume*6./np.pi))
+            print n
+            for i in range(n):
+                P = positions.CartesianPoint(self.radius*np.random.uniform(-1., 1.), self.radius*np.random.uniform(-1., 1.), self.radius*np.random.uniform(-1., 1.))
+                if P.r <= self.radius:
+                    self.points.append(P)
+            self.Npoints = len(self.points)
+            print self.Npoints
+            if self.Npoints <= 1:
+                self.points = np.array([positions.SeismoPoint(0.,0.,0.)])
+                self.Npoints = 1
+                
 
     #def translation(self, velocity, dt):
     #    """ translation of the point.
@@ -62,13 +138,13 @@ class Model():
     #       """
     #       self.new.x = self.old.x + velocity*dt
 
-class Translation(Point_evolution):
+class Translation(Model):
     ## Not up-to-date
     ### the __init__ method is directly obtained from the class Point_evolution, so no need to redefined it in a derived class
     ## def __init__(self, Point, time)
     ##     Point_evolution.__init__(self, Point, time)
 
-    def advection(self, velocity):
+    def advect(self, velocity):
         """ advection of the point. """
         self.old = self.new
         translation(self, velocity, dt)
@@ -85,8 +161,10 @@ if __name__ == '__main__':
 
     
     
-    A = positions.CartesianPoint(1200,0,0)
-    Point_evolution(A, 1)
-    B = Translation(A,1)
-    B.analytical(2000, 1)
-    
+    A = Model()
+    print A.Npoints
+    A.initialisation(1., 10.,1.) 
+    print A.Npoints
+    x, y, z = A.extract_xyz()
+    plt.plot(np.sqrt(x**2+y**2+z**2), '.')
+    plt.show()
