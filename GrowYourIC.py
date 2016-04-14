4e01f20b1d5f060a3fc31bac1ea53181297482b6
MarineLasbleis <marine.lasbleis@gmail.com>
2016-04-14 10:20:22 +0900

    add GrowYouIC.py to act as the main python file.

---
+++ GrowYourIC.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python
+# Project : From geodynamic to Seismic observations in the Earth's inner core
+# Author : Marine Lasbleis
+
+import numpy as np
+import matplotlib.pyplot as plt #for figures
+from mpl_toolkits.basemap import Basemap #to render maps
+import math
+
+import positions
+import geodynamic
+import plot_data
+import data
+
+## Choose a model in the list: 
+    # geodynamic.PureTranslation()
+
+
+## Choose a data set:
+    # data.SeismicFromFile(filename)
+        # plot function is : 
+    # data.PerfectSamplingEquator(numbers_of_points)
+        # plot function is : 
+
+
+
+
+if __name__ == '__main__':
+
+    pass
