d777bb0e68d2af7b5a5293a67c9a198a1881010b
MarineLasbleis <marine.lasbleis@gmail.com>
2016-08-30 16:32:41 +0900

    pep8 compliance

--- geodyn.py
+++ geodyn.py
@@ -2,20 +2,27 @@
 # Project : From geodynamic to Seismic observations in the Earth's inner core
 # Author : Marine Lasbleis
 
+""" geodyn.py
+
+Define the base class for geodynamical models
+
+CLass:
+    Model
+        def set_parameters
+        def verification
+        def proxy_singlepoint: NotImplemented
+        def define_units
+Functions:
+    evaluate_proxy: evaluate proxy over all points of the data set using the geodyn model.
+    average_proxy: method to average proxy over raypath
+"""
 
 import numpy as np
-import matplotlib.pyplot as plt  # for figures
-from mpl_toolkits.basemap import Basemap  # to render maps
-import math
-from scipy.integrate import ode
-from scipy.optimize import fsolve
-
-# personal routines
-#import positions
-#import intersection
 
 
-class Model():
+class Model(object):
+    """Base class for all the geodynamical models"""
+
 
     def set_parameters(self, dict_param):
         """ add any parameter of the form {'param':value} as self.param = value """
@@ -23,11 +30,14 @@ class Model():
             setattr(self, k, v)
 
     def verification(self):
-        """ verify if the geodynamical model verify some very simple assumptions (ex: non zero radius, translation velocity fast enough if case without growth, etc.) 
+        """Verify if everything required is here.
+
+        Verify if the geodynamical model verify some very simple assumptions
+        (ex: non zero radius, translation velocity fast enough if case without growth, etc.)
         this method has to be implemented in derived class.
         By default, no verification is done."""
-        self.units()
-        print "No verification has been implemented for this geodynamical model. If needed, please implement them in the class."
+        raise NotImplementedError(
+            "need to implement verification() in derived class!")
 
     def proxy_singlepoint(self, point):
         """ evaluate the proxy on a single positions.Point instance."""
@@ -35,6 +45,7 @@ class Model():
             "need to implement proxy_singlepoint() in derived class!")
 
     def define_units(self):
+        """Define the units"""
         # parameters have been given in non-dimensional forms.
         if self.units == None:
             # if units have been given separately
@@ -57,13 +68,12 @@ class Model():
         elif self.units == ("vt", "rICB"):
             self.time_unit, self.length_unit = self.rICB / self.vt, self.rICB
         else:
-            time_unit, length_unit = 1e9, 1.221e6
-
+            self.time_unit, self.length_unit = 1e9, 1.221e6
            # if no units have been given, we set them arbitrarly
 
 
 def evaluate_proxy(dataset, method, verbose=True):
-    """ evaluate the value of the proxy on all the points of the data set, using the choosen geodynamical method
+    """ evaluate the value of the proxy on all the points of the data set
 
         dataset : a data.SeismicData object
         method : a geodyn.Model  object
@@ -94,8 +104,8 @@ def evaluate_proxy(dataset, method, verbose=True):
             # but if we want to compute the velocity of Pwaves,
             # we may want to compute the velocity for each point
             # and average over the inverse of the velocities.
-            N = dataset.NpointsRaypath
-            dataset.data_points[i].straigth_in_out(N + 2)
+            number_points = dataset.NpointsRaypath
+            dataset.data_points[i].straigth_in_out(number_points + 2)
             raypath = ray.points
             proxy[i] = average_proxy(raypath, method)
     print "==="
@@ -103,23 +113,23 @@ def evaluate_proxy(dataset, method, verbose=True):
 
 
 def average_proxy(ray, method):
-    """ method to average proxy over the raypath.
+    r""" method to average proxy over the raypath.
 
-    Simple method is direct average of the proxy: \sum proxy(r) / \sum dr.
-    Other methods could be: 1/(\sum 1/proxy) (better for computing \delta t)
+    Simple method is direct average of the proxy: $\sum proxy(r) / \sum dr$.
+    Other methods could be: $1/(\sum 1 / proxy)$ (better for computing \delta t)
     """
     total_proxy = 0.
     if method.evaluation == "inverse":
-        for j, point in enumerate(ray):
+        for _, point in enumerate(ray):
             _proxy = method.proxy_singlepoint(point)[method.proxy_type]
             total_proxy += 1. / _proxy
-        N = len(ray)
-        proxy = 1. / total_proxy / float(N)
+        number = len(ray)
+        proxy = 1. / total_proxy / float(number)
     else:
         for j, point in enumerate(ray):
             _proxy = method.proxy_singlepoint(point)[method.proxy_type]
             total_proxy += _proxy
-        N = len(ray)
-        proxy = total_proxy / float(N)
+        number = len(ray)
+        proxy = total_proxy / float(number)
 
     return proxy
