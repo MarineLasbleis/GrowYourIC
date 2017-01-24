df726a3d2c5512180f05e9fb9c013c8af6bbf769
MarineLasbleis <marine.lasbleis@gmail.com>
2017-01-24 22:00:09 +0900

    update __init__

---
+++ GrowYourIC/__init__.py
@@ -0,0 +1,20 @@
+""" 
+GrowYourIC
+====
+
+GrowYourIC is a tool to model seismic observation through inner core geodynamical modelisation.
+
+"""
+
+from __future__ import absolute_import
+__version__ = "0.1.0"
+
+from . import data
+from . import geodyn
+from . import intersection
+from . import positions
+from . import plot_data
+from . import mineral_phys_data
+from . import geodyn_trg
+from . import geodyn_static
+from . import cross_sections
