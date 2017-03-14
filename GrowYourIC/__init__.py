c55c0f91bf2af469fa39ab94dc28b91ba1b6ef4b
MarineLasbleis <marine.lasbleis@gmail.com>
2017-03-14 11:50:30 +0900

    move test folder

--- GrowYourIC/__init__.py
+++ GrowYourIC/__init__.py
@@ -7,7 +7,7 @@ GrowYourIC is a tool to model seismic observation through inner core geodynamica
 """
 
 from __future__ import absolute_import
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from . import data
 from . import geodyn
