7a46f54f17ea4d79a538ec24348b2e0d0414b27d
MarineLasbleis <marine.lasbleis@gmail.com>
2018-04-21 19:10:51 +0200

    new model with radial dependence only (to check innermost inner core

--- GrowYourIC/geodyn_static.py
+++ GrowYourIC/geodyn_static.py
@@ -41,3 +41,38 @@ class Hemispheres(geodyn.Model):
 
     def verification(self):
         pass
+
+
+
+
+
+
+class Radial_sym(geodyn.Model):
+    """ Static hemispheres: 
+
+        proxy is just defines as -1 in the western hemisphere and +1 in the eastern one."""
+
+    def __init__(self, fonction=None):
+        self.name = "Radial symmetry"
+
+        if fonction == None:
+            def fonction(r):
+                return r
+        self.function_radius = fonction #has to be a function with 1 argument (radius)
+
+    def proxy_singlepoint(self, point, proxy_type):
+        """  """
+        proxy = {}  # empty dict
+        proxy["radius"] = self.function_radius(point.r)
+        #np.sign(np.sin((point.phi + self.anglephi) * np.pi / 180.))
+        return proxy
+
+    def velocity(self, time, point):
+        """ has to be defined, but set to 0 """
+        return [0., 0., 0.]
+
+    def radius_ic(self, t):
+        return self.rICB
+
+    def verification(self):
+        pass
